8707dceca0816bdd36a623bdf475b4548a5305b3
Lautaro Rocca <lrocca2019@gmail.com>
2023-04-13 12:09:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ import Ejercicio10
 while True:
     os.system('cls') #limpia pantalla
     print('Bienvenidos')
+    print('1- Transformar formato dd/mm/yyyy a yyyy/mm/dd')
+    print('2- Mostrar hora local')
+    print('3- Contar las palabras de un texto')
+    print('4- Mostrar horario en formato 24hs')
+    print('5- Reescribir el inverso de un texto')
+    print('6- Calcula la suma de todos los numeros hasta el ingresado')
+    print('7- Escriba una palabra y la separamos en letras')
+    print('8- Acepta un texto y devuelve las palabras con mas de 5 letras')
+    print('9- Ingresa un texto y lo devuelve reemplazando la palabra clave por otra ingresada')
+    print('10- Ingresa una serie de palabras y devuelve aquellas que contengan una vocal')
+    print('11- Ingresa una lista de enteros y devuelve aquellos divisibles por 3')
     opcion = input('seleccione programa a ejecutar: ')
     if opcion == '1':
       Ex1.principal()
