da916d90be3d97917f6b3a543598c978c2bb11b3
Lautaro Rocca <lrocca2019@gmail.com>
2023-04-13 11:35:33 +0200

    Fix main

--- Ex4.py
+++ Ex4.py
@@ -1,32 +1,32 @@
 # Crea un programa que le pida al usuario una hora en formato "hh:mm" y luego calcule y muestre la hora en 24 horas (por ejemplo, "19:30" se convertiría en "19:30").
  ## La hora debe ser validada para asegurarse de que esté en el formato correcto y de que las horas y los minutos sean números enteros.
+def principal():
+    while True:
+        timeStr = input("Enter a time in hh:mm format: ")
+        try:
+            hours, minutes = timeStr.split(":")
+            hours = int(hours)
+            minutes = int(minutes)
 
-while True:
-    timeStr = input("Enter a time in hh:mm format: ")
-    try:
-        hours, minutes = timeStr.split(":")
-        hours = int(hours)
-        minutes = int(minutes)
-        
-        if hours < 0 or hours > 23 or minutes < 0 or minutes > 59:
-            print("Invalid input. Please enter a valid time in hh:mm format.")
+            if hours < 0 or hours > 23 or minutes < 0 or minutes > 59:
+                print("Invalid input. Please enter a valid time in hh:mm format.")
+                continue
+            break
+        except ValueError:
+            print("Value error. Please enter a valid time in hh:mm format.")
             continue
-        break
-    except ValueError:
-        print("Value error. Please enter a valid time in hh:mm format.")
-        continue
 
-if hours < 12 and hours >= 1:
-    period = "AM"
-else:
-    period = "PM"
+    if hours < 12 and hours >= 1:
+        period = "AM"
+    else:
+        period = "PM"
 
-if period == "PM" and hours != 12:
-    hours -= 12
+    if period == "PM" and hours != 12:
+        hours -= 12
 
-if hours < 10:
-    hours = "0" + str(hours)
-if minutes < 10:
-    minutes = "0" + str(minutes)
+    if hours < 10:
+        hours = "0" + str(hours)
+    if minutes < 10:
+        minutes = "0" + str(minutes)
 
-print(f"{hours}:{minutes}")
+    print(f"{hours}:{minutes}")
--- main.py
+++ main.py
@@ -15,3 +15,30 @@ import Ejercicio10
 while True:
     os.system('cls') #limpia pantalla
     print('Bienvenidos')
+    opcion = input('seleccione programa a ejecutar: ')
+    if opcion == '1':
+      Ex1.principal()
+    elif opcion == '2':
+      Ex2.principal()
+    elif opcion == '3':
+        Ex3.principal()
+    elif opcion == '4':
+      Ex4.principal()
+    elif opcion == '5':
+      Ex5.principal()
+    elif opcion == '6':
+      Ejercicio6.main()
+    elif opcion == '7':
+      Ejercicio7.main()
+    elif opcion == '8':
+      Ejercicio8.main()
+    elif opcion == '9':
+      Ejercicio9.main()
+    elif opcion == '10':
+      Ejercicio10.main()
+    elif opcion == '11':
+      Ejercicio11.main()
+    elif opcion == "0":
+      print('Un placer, hasta la proxima')
+      break
+    continuar = input('presione enter para continuar')
